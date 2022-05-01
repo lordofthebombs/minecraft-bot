51f78ff8c4d8cd784fc72162d1534e33c02d1f59
lordofthebombs <25441886+lordofthebombs@users.noreply.github.com>
2022-05-01 16:33:27 -0600

    New start to main

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,17 @@
 import { Client, Intents } from "discord.js";
 
+// Loading in bot token
 const token = require("../secrets.json").token;
+
+// Creatomg client instance
+const client: Client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+
+console.log("Starting bot...")
+
+// When client is ready, run this code only once
+client.once("ready", (): void => {
+    console.log(`${client.user?.username} is online!`)
+})
+
+// Logging in
+client.login(token);
