b7af93e113d3c3dca2712637ae5e7c9f66a64adf
lordofthebombs <25441886+lordofthebombs@users.noreply.github.com>
2022-05-01 20:22:03 -0600

    Added semicolon but probably doesnt matter lol

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ console.log("Starting bot...")
 
 // When client is ready, run this code only once
 client.once("ready", (): void => {
-    console.log(`${client.user?.username} is online!`)
+    console.log(`${client.user?.username} is online!`);
 })
 
 // Logging in
